10099d2528717441aa55fce802c39026d68c26c4
bobCodeContributor <152165892+bobCodeContributor@users.noreply.github.com>
2023-11-27 12:06:26 +0200

    implement pow

--- numbers.go
+++ numbers.go
@@ -23,3 +23,11 @@ func Mul3(n1, n2, n3 int) int {
 func Add3(n1 int, n2 int, n3 int) int {
 	return n1 + n2 + n3
 }
+
+func Pow(n1, n2 int) int {
+	out := 1
+	for i := 0; i < n1; i++ {
+		out = Mul(out, n2)
+	}
+	return out
+}
