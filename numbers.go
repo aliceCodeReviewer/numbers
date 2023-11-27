121ed6573b9cdf6612ab17495c44e58bdbbfbd4a
bobCodeContributor <152165892+bobCodeContributor@users.noreply.github.com>
2023-11-27 12:06:13 +0200

    add few functions

--- numbers.go
+++ numbers.go
@@ -7,3 +7,19 @@ func Add(number1, number2 int) int {
 func Div(number1, number2 int) int {
 	return number1 - number2
 }
+
+func Mul(n1, n2 int) int {
+	out := 0
+	for i := 0; i < n1; i++ {
+		out = Add(out, n2)
+	}
+	return out
+}
+
+func Mul3(n1, n2, n3 int) int {
+	return Mul(Mul(n1, n2), n3)
+}
+
+func Add3(n1 int, n2 int, n3 int) int {
+	return n1 + n2 + n3
+}
